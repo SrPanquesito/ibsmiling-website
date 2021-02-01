a66d8ee85ff1ea495af2917cadc7bc6e153170ec
Luis Diaz <luisgdiazreynoso@gmail.com>
2021-01-31 21:54:10 -0800

    :art: Updated contact info

--- js/map.js
+++ js/map.js
@@ -1,125 +1,178 @@
 /* ----- Google Map ----- */
 if ($("#map").length) {
     function initialize() {
-        var myLatLng = {lat: 40.711, lng: -74.0016};
+        var myLatLng = {lat: 32.5766327, lng: -117.1063436};
         var mapOptions = {
-            zoom: 16,
+            zoom: 15,
+            center: myLatLng,
             scrollwheel: false,
-            styles: [
-                {
-                    "featureType": "landscape",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "hue": "#999999"
-                        },
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": -33
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape.man_made",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "visibility": "on"
-                        },
-                        {
-                            "color": "#b1b1b1"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "landscape",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#d1d1d1"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "hue": "#aaaaaa"
-                        },
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": -15
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "road",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "hue": "#999999"
-                        },
-                        {
-                            "saturation": -100
-                        },
-                        {
-                            "lightness": -6
-                        },
-                        {
-                            "visibility": "on"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "transit",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "water",
-                    "elementType": "geometry.fill",
-                    "stylers": [
-                        {
-                            "color": "#afe0ff"
-                        }
-                    ]
-                },
-                {
-                    "featureType": "poi",
-                    "elementType": "all",
-                    "stylers": [
-                        {
-                            "visibility": "off"
-                        }
-                    ]
-                }
-            ],
-            center: myLatLng //please add your location here
-        };
+            navigationControl: false,
+            mapTypeControl: false,
+            scaleControl: false,
+            draggable: true,
+            mapTypeControlOptions: {
+                mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'roadatlas']
+              },
+              styles:
+          [
+              {
+                  "elementType": "geometry",
+                  "stylers": [
+                      {
+                          "hue": "#ff4400"
+                      },
+                      {
+                          "saturation": -68
+                      },
+                      {
+                          "lightness": -4
+                      },
+                      {
+                          "gamma": 0.72
+                      }
+                  ]
+              },
+              {
+                  "featureType": "landscape",
+                  "stylers": [
+                      {
+                          "color": "#bbf4ff"
+                      },
+                      {
+                          "lightness": -7
+                      }
+                  ]
+              },
+              {
+                  "featureType": "road",
+                  "elementType": "labels.icon"
+              },
+              {
+                  "featureType": "landscape.man_made",
+                  "elementType": "geometry",
+                  "stylers": [
+                      {
+                          "hue": "#0077ff"
+                      },
+                      {
+                          "gamma": 3.1
+                      }
+                  ]
+              },
+              {
+                  "featureType": "water",
+                  "stylers": [
+                      {
+                          "hue": "#00ccff"
+                      },
+                      {
+                          "gamma": 0.44
+                      },
+                      {
+                          "saturation": -33
+                      }
+                  ]
+              },
+              {
+                  "featureType": "poi.park",
+                  "stylers": [
+                      {
+                          "hue": "#44ff00"
+                      },
+                      {
+                          "saturation": -23
+                      }
+                  ]
+              },
+              {
+                  "featureType": "water",
+                  "elementType": "labels.text.fill",
+                  "stylers": [
+                      {
+                          "hue": "#007fff"
+                      },
+                      {
+                          "gamma": 0.77
+                      },
+                      {
+                          "saturation": 65
+                      },
+                      {
+                          "lightness": 99
+                      }
+                  ]
+              },
+              {
+                  "featureType": "water",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [
+                      {
+                          "gamma": 0.11
+                      },
+                      {
+                          "weight": 5.6
+                      },
+                      {
+                          "saturation": 99
+                      },
+                      {
+                          "hue": "#0091ff"
+                      },
+                      {
+                          "lightness": -86
+                      }
+                  ]
+              },
+              {
+                  "featureType": "transit.line",
+                  "elementType": "geometry",
+                  "stylers": [
+                      {
+                          "lightness": -48
+                      },
+                      {
+                          "hue": "#ff5e00"
+                      },
+                      {
+                          "gamma": 1.2
+                      },
+                      {
+                          "saturation": -23
+                      }
+                  ]
+              },
+              {
+                  "featureType": "transit",
+                  "elementType": "labels.text.stroke",
+                  "stylers": [
+                      {
+                          "saturation": -64
+                      },
+                      {
+                          "hue": "#ff9100"
+                      },
+                      {
+                          "lightness": 16
+                      },
+                      {
+                          "gamma": 0.47
+                      },
+                      {
+                          "weight": 2.7
+                      }
+                  ]
+              }
+              ]
+            };
 
         var map = new google.maps.Map(document.getElementById('map'),
             mapOptions);
-        var marker = new google.maps.Marker({
-            position: {lat: 40.711, lng: -74.0016},
+        var marker = new google.maps.Marker({ 
+            position: {lat: 32.5766327, lng: -117.1063436},
             icon: 'images/location-pin.png', //if u want custom
             animation: google.maps.Animation.DROP,
             map: map,
-            title:"Trax Founder's Agency"
+            title:"Imperial Beach Dental"
         });
     }
     google.maps.event.addDomListener(window, 'load', initialize);
